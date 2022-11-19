8d5825963d9c8dd70ea5cda523c7b816745cb570
nvhung1810 <coannhonbeo@gmail.com>
2022-11-19 13:16:20 +0700

    .

--- spring_angular/spring_backend/src/main/java/com/example/spring_backend/repository/ValuesRepository.java
+++ spring_angular/spring_backend/src/main/java/com/example/spring_backend/repository/ValuesRepository.java
@@ -1,14 +1,4 @@
 package com.example.spring_backend.repository;
 
-import com.example.spring_backend.model.Values;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.List;
-
-public interface ValuesRepository extends JpaRepository<Values, Long> {
-//    List<Values> findByPublished(boolean published);
-
-//    Phải là fintitleContaining thì mới tìm kiếm bên fontend  được
-    List<Values> findByTenhang_hungContaining(String tenhang_hung);
+public interface ValuesRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.spring_backend.model.Values, long> {
 }
